2a0e419edd7e0e846256d4e8778a4ae3ce3dfa07
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-05 23:35:54 -0500

    added operator functions

---
+++ server/public/scripts/client.js
@@ -0,0 +1,81 @@
+$( document ).ready( onReady );
+
+function onReady(){
+    refreshMath();
+    $( document ).on( 'click', '#additionBtn', onAddition );
+    $( document ).on( 'click', '#subtractionBtn', onSubtraction );
+    $( document ).on( 'click', '#multiplicationBtn', onMultiplication );
+    $( document ).on( 'click', '#divisionBtn', onDivision );
+    $( document ).on( 'click', '#equalsBtn', onEquals );
+    //$( document ).on( 'click', '#clearBtn', onClear );
+} // end onReady
+
+//-------------------------------------------------------------
+
+let operator;
+function onAddition(){
+    operator = '+'
+} // end onAddition
+function onSubtraction(){
+    operator = '-'
+} // end onSubtraction
+function onMultiplication(){
+    operator = '*'
+} // end onMultiplication
+function onDivision(){
+    operator = '/'
+} // end onDivision
+
+//-------------------------------------------------------------
+/*
+function refreshMath(){
+    $.ajax( {
+        url: '/mathematics',
+        method: 'GET'
+    } ).then( function( response ){
+        console.log( 'We got a response:', response );
+        let el = $( '#answerOut' );
+        let element = $( '.listOut' );
+        element.empty();
+        for( let i = 0; i < response.length; i++ ){
+            $( '.listOut' ).append( `
+                <li>
+                    ${response[ i ].inputOne}
+                    ${response[ i ].operator}
+                    ${response[ i ].inputTwo}
+                    =
+                    ${response[ i ].answer}
+                </li>
+            ` );
+            el.empty();
+            $( '#answerOut' ).append( `
+                <h2>
+                    ${response[ i ].answer}
+                </h2>
+            ` );
+        } // end for
+    } ).catch( function( errorInfo ){
+        alert( 'Error in ajax GET', errorInfo );
+    } ); // end AJAX
+} // refreshMath
+
+//-------------------------------------------------------------
+
+function onEquals(){
+    let objectToSend = {
+        inputOne: $( '#firstInput' ).val(),
+        operator: operator,
+        inputTwo: $( '#secondInput' ).val()
+    } // end newMath
+    $.ajax( {
+        url: '/mathematics',
+        method: 'POST',
+        data: objectToSend
+    } ).then( function( response ) {
+        console.log( 'Created a math problem:', response );
+        refreshMath();
+    } ).catch( function( errorInfo ){
+        alert( 'Error in ajax POST', errorInfo );
+    } ); // end AJAX
+} // end onEquals
+*/
--- server/server.js
+++ server/server.js
@@ -5,7 +5,60 @@ const PORT = 3000;
 
 app.use( express.static( 'server/public' ) );
 app.use( bodyParser.urlencoded( { extended: true } ) );
+/*
+let math = [];
 
-app.listen( PORT, function(){
+//-------------------------------------------------------------
+
+// GET
+app.get( '/mathematics', ( req, res ) => {
+    console.log( 'inside of app.get!' );
+    console.log( 'what is math?', math );
+    res.send( math );
+} ); // end GET
+
+//-------------------------------------------------------------
+
+// POST
+app.post( '/mathematics', ( req, res ) => {
+    let objectToSend = req.body;
+    calculations( objectToSend );
+
+    function calculations(){
+        let equation;
+        if( objectToSend.operator === '+' ){
+            console.log( 'adding numbers' );
+            equation = Number( objectToSend.inputOne ) + Number( objectToSend.inputTwo );
+        } // end if
+        else if( objectToSend.operator === '-' ){
+            console.log( 'subtracting numbers' );
+            equation =  Number( objectToSend.inputOne ) - Number( objectToSend.inputTwo );
+        } // end else if
+        else if( objectToSend.operator === '*' ){
+            console.log( 'multiplying numbers' );
+            equation = Number( objectToSend.inputOne ) * Number( objectToSend.inputTwo );
+        } // end else if
+        else if( objectToSend.operator === '/' ){
+            console.log( 'dividing numbers' );
+            equation = Number( objectToSend.inputOne ) / Number( objectToSend.inputTwo );
+        } // end else if
+        else{
+            console.log( 'Somethings wrong in calculation' );
+        } // end else
+        console.log( 'Answer is:', equation );
+        objectToSend.answer = equation;
+        math.push( objectToSend );
+        
+    } // end calculations
+    
+    console.log( 'req.body is:', req.body );
+    res.sendStatus( 200 );
+} ); // end POST
+
+//-------------------------------------------------------------
+
+// listen
+app.listen( PORT, () => {
     console.log( 'Node listening at port:', PORT );
-} );
+} ); // end listen
+*/
